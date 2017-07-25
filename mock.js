926a69550ac57aad36b34960f7d1ee2098f5d355
TightenPreacher <731995484@qq.com>
2017-07-25 18:20:48 +0800

    dd

---
+++ mock.js
@@ -0,0 +1,19 @@
+Mock.mock('打印机型', {
+    data: {
+        outdor: {
+            name: 'hjc',
+            imgUrl: '123456',
+            tagUrl: '123'
+        },
+        epuson: {
+            name: 'hjc',
+            imgUrl: '123456',
+            tagUrl: '123'
+        },
+        liguang: {
+            name: 'hjc',
+            imgUrl: '123456',
+            tagUrl: '123'
+        }
+    }
+});
